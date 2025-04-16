0422d20e75984cee4258db3e289a39b992e8dedb
Sahil Ahmed <sahilahmed.new24@gmail.com>
2025-04-16 18:06:12 +0530

    Fix: Fix Category and subcategory headings

--- client/src/pages/DashBoard.jsx
+++ client/src/pages/DashBoard.jsx
@@ -59,14 +59,14 @@ export const DashBoard = () => {
       </div>
 
       <div className="grid grid-cols-2 gap-6 mt-10">
-        <div className="flex items-center justify-center flex-col">
-          <h2 className="text-xl font-semibold mb-4">Category Distribution</h2>
+        <div className="mt-2">
+          <h2 className="text-xl font-semibold mb-4">SubCategory Distribution</h2>
           <PieChartBySubCategory data={FinalData} />
         </div>
 
         <div className="mt-2">
             <h3 className="text-lg font-semibold mb-2">
-              SubCategory Breakdown
+              Category Breakdown
             </h3>
             <PieChartByCategory data={FinalData} />
           </div>
